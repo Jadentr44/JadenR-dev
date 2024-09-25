8033fa442abb77be666d1862562511f8098b136a
Jaden <jadentr.44@gmail.com>
2024-09-25 00:23:43 -0500

    added contact

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
+import Contact from "./components/Contact.jsx";
 import Projects from "./components/Projects.jsx";
 import { useState, useEffect } from "react";
 
@@ -21,13 +22,15 @@ export default function Home() {
 
   return (
     <main
-      className={`${theme} bg-mainBg dark:bg-gray-600 text-black dark:text-white overflow-x-hidden scrollbar-none`}
+      className={`${theme} bg-mainBg dark:bg-gray-600 text-black dark:text-white overflow-hidden `}
     >
       <Navbar toggle={toggleDarkMode} theme={theme} />
 
       <Hero />
 
       <Projects />
+
+      <Contact />
     </main>
   );
 }
